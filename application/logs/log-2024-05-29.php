86a1ae29ddb15663f4a71157a5247bbb3d53b3ed
DiaryRz <119029048+DiaryRz@users.noreply.github.com>
2024-05-30 13:27:32 +0200

    logs

--- application/logs/log-2024-05-29.php
+++ application/logs/log-2024-05-29.php
@@ -86,3 +86,7 @@ ERROR - 2024-05-29 11:00:57 --> Severity: Warning --> Undefined array key 2 C:\x
 ERROR - 2024-05-29 11:00:57 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated C:\xampp\htdocs\S6\Construction\application\models\ImportModel.php 340
 ERROR - 2024-05-29 11:00:57 --> Severity: Warning --> Undefined array key 3 C:\xampp\htdocs\S6\Construction\application\models\ImportModel.php 341
 ERROR - 2024-05-29 11:00:57 --> Severity: 8192 --> trim(): Passing null to parameter #1 ($string) of type string is deprecated C:\xampp\htdocs\S6\Construction\application\models\ImportModel.php 341
+ERROR - 2024-05-29 11:14:03 --> 404 Page Not Found: Assets/bootstrap-5.0.2-dist
+ERROR - 2024-05-29 11:14:36 --> 404 Page Not Found: ListeController/recherche
+ERROR - 2024-05-29 11:15:01 --> 404 Page Not Found: Assets/images
+ERROR - 2024-05-29 11:15:01 --> 404 Page Not Found: Assets/images
---
+++ application/logs/log-2024-05-30.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-05-30 09:28:23 --> 404 Page Not Found: Assets/bootstrap-5.0.2-dist
+ERROR - 2024-05-30 09:28:36 --> 404 Page Not Found: Assets/bootstrap-5.0.2-dist
+ERROR - 2024-05-30 09:28:44 --> 404 Page Not Found: Assets/bootstrap-5.0.2-dist
+ERROR - 2024-05-30 09:29:40 --> 404 Page Not Found: Assets/bootstrap-5.0.2-dist
+ERROR - 2024-05-30 09:29:49 --> 404 Page Not Found: Assets/bootstrap-5.0.2-dist
